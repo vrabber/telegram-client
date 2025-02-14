b81760e27d3d4749fb1d8ae1f65325879d69cec8
bonefabric <iburanguloff@yandex.ru>
2025-02-14 20:10:49 +0500

    vrabber server connection config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,11 +5,18 @@ import (
 )
 
 type Config struct {
-	TgToken string `envconfig:"TG_TOKEN" required:"true"`
+	TgToken    string `envconfig:"TG_TOKEN" required:"true"`
+	ServerHost string `envconfig:"VRABBER_HOST" default:"localhost"`
+	ServerPort int    `envconfig:"VRABBER_PORT" required:"true"`
 }
 
 func MustLoad() *Config {
 	var config Config
 	envconfig.MustProcess("", &config)
+
+	if config.TgToken == "" {
+		panic("TG_TOKEN env variable must not be empty")
+	}
+
 	return &config
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -2,31 +2,65 @@ package config
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
 func TestLoad(t *testing.T) {
 	cases := []struct {
-		name  string
-		value string
+		name        string
+		token       string
+		vrabberHost string
+		vrabberPort string
+		mustPanic   bool
 	}{
 		{
-			name:  "empty",
-			value: "",
+			name:        "correct",
+			token:       "test_token",
+			vrabberHost: "localhost",
+			vrabberPort: "9090",
+			mustPanic:   false,
 		},
 		{
-			name:  "normal",
-			value: "test_value",
+			name:        "empty token",
+			token:       "",
+			vrabberHost: "localhost",
+			vrabberPort: "9090",
+			mustPanic:   true,
+		},
+		{
+			name:        "incorrect port",
+			token:       "test_token",
+			vrabberHost: "localhost",
+			vrabberPort: "-10",
 		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			t.Setenv("TG_TOKEN", c.value)
-			c, err := Load()
-			assert.Nil(t, err)
+			t.Setenv("TG_TOKEN", c.token)
+			t.Setenv("VRABBER_HOST", c.vrabberHost)
+			t.Setenv("VRABBER_PORT", c.vrabberPort)
+
+			defer func() {
+				err := recover()
+
+				if c.mustPanic {
+					assert.NotNil(t, err)
+				} else {
+					assert.Nil(t, err)
+				}
+			}()
+
+			conf := MustLoad()
 			assert.NotNil(t, c)
-			assert.Equal(t, c.TgToken, c.TgToken)
+
+			assert.Equal(t, c.token, conf.TgToken)
+			assert.Equal(t, c.vrabberHost, conf.ServerHost)
+
+			p, err := strconv.Atoi(c.vrabberPort)
+			assert.Nil(t, err)
+			assert.Equal(t, p, conf.ServerPort)
 		})
 	}
 }
