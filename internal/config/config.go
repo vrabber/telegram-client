976cbead12a5fbe355e007b8d254576d7d965ae5
bonefabric <iburanguloff@yandex.ru>
2025-02-14 18:38:43 +0500

    upd local config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,15 @@
 package config
 
 import (
-	"fmt"
 	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	TgToken string
+	TgToken string `envconfig:"TG_TOKEN" required:"true"`
 }
 
-func Load() (*Config, error) {
+func MustLoad() *Config {
 	var config Config
-	err := envconfig.Process("", &config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to process env vars: %v", err)
-	}
-	return &config, nil
+	envconfig.MustProcess("", &config)
+	return &config
 }
