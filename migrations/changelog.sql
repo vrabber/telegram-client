1eb3abb575293e8772c7268f91c4ce3b279ab6e6
bonefabric <iburanguloff@yandex.ru>
2025-02-27 07:01:51 +0500

    [VRA-8] Подключение хранилища

--- migrations/changelog.sql
+++ migrations/changelog.sql
@@ -2,7 +2,9 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id         SERIAL PRIMARY KEY,
+    telegram_user_id INT UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT (NOW()),
     updated_at TIMESTAMP NOT NULL DEFAULT (NOW())
         CONSTRAINT updated_at_valid CHECK ( updated_at >= created_at )
-)
+);
+--rollback DROP TABLE IF EXISTS users;
